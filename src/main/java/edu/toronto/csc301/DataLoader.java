957d27d12ecb713edefbc11c75deb002b6087e74
Akshay <akshayjain1996@gmail.com>
2015-09-24 21:53:34 -0400

    Implemented Interfaces. Wrote trivial code for Tweet class

---
+++ src/main/java/edu/toronto/csc301/DataLoader.java
@@ -0,0 +1,15 @@
+package edu.toronto.csc301;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class DataLoader implements IDataLoader{
+
+	@Override
+	public List<ITweet> load(InputStream data) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
