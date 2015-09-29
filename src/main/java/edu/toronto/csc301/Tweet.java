f0b4748c56f2831b07d42199d9efbd989d0219af
Akshay <akshayjain1996@gmail.com>
2015-09-28 22:31:36 -0400

    Fixed Tweet class to work for all test cases

--- src/main/java/edu/toronto/csc301/Tweet.java
+++ src/main/java/edu/toronto/csc301/Tweet.java
@@ -1,5 +1,6 @@
 package edu.toronto.csc301;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Tweet implements ITweet{
@@ -8,8 +9,9 @@ public class Tweet implements ITweet{
 	private String text;
 
 	public Tweet (String username, String text){
-		this.username = username;
-		this.text = text;
+		setUsername(username);
+		setText(text);
+		
 	}
 	
 	public Tweet() {
@@ -50,18 +52,41 @@ public class Tweet implements ITweet{
 			throw new IllegalArgumentException();
 		}
 		
+		if(text.length() > 140){
+			throw new IllegalArgumentException();
+		}
+		
 		this.text = text;
 		//Checks Complete. Add rest of the functionality.
 	}
 
 	@Override
 	public Set<String> getHashTags() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<String> hashTags = new HashSet<>();
+		String [] textParts = text.split(" ");
+		for (int i = 0; i < textParts.length; i++){
+			String part = textParts[i].trim();
+			if(part.startsWith("#")){
+				String textWithoutHash = part.substring(1);
+				if(textWithoutHash.length() > 0){
+					String[] words = textWithoutHash.split("[^a-zA-Z0-9_']+");
+					hashTags.add(words[0]);
+				}
+			}
+		}
+		
+		return hashTags;
 	}
 
 	
 	public boolean checkLegalUsername(String username){
+		if(username.length() > 32){
+			return false;
+		}
+		if(username.length() == 0){
+			return false;
+		}
+		
 		for(int i = 0; i < username.length(); i++){
 			char c = username.charAt(i);
 			if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')) || ((c >= 0) && (c <= 9)) || (c == '_')){
@@ -70,5 +95,17 @@ public class Tweet implements ITweet{
 			}
 		}
 		return true;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(Tweet.class.isAssignableFrom(obj.getClass()))){
+			return false;
+		}
+		Tweet tweet = (Tweet) obj;
+		if(tweet.getUsername().equals(this.username) && (tweet.getText().equals(text))){
+			return true;
+		}
+		return false;
 	}
 }
